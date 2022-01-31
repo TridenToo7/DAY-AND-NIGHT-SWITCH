766966410f4b1a6c3d8d8474b2f44b792f1e02b2
TridenT <TridenToo7>
2022-01-31 13:46:54 +0600

    day and night switch

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHome(),
+    );
+  }
+}
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  bool _switch =false;
+  ThemeData _dark = ThemeData(brightness: Brightness.dark);
+  ThemeData light = ThemeData(brightness: Brightness.light);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: _switch ? _dark : light,
+      home: Scaffold(
+        backgroundColor: Color(0xffD1B25F),
+        appBar: AppBar(
+          backgroundColor: Colors.black38,
+          title: Text ('MONOAR HOSSAIN'),
+          centerTitle: true,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(bottomLeft:Radius.circular(30) ,bottomRight: Radius.circular(30) ),
+            side: BorderSide(
+                width: 5,
+                color: Colors.black38
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.lightbulb,size: 55,color: Colors.yellowAccent,),
+              SizedBox(height: 30,),
+              Text("Switch",
+                style: TextStyle(fontSize: 40,),),
+              SizedBox(
+                height:  20,
+              ),
+              Switch(value: _switch, onChanged: (_newvalue){
+                setState(() {
+                  _switch = _newvalue;
+                });
+              })
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
+
